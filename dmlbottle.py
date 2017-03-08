ffbbf90e6eebacbdd6b26800d3a9b97b5fb5853a
yobmod <yobmod@gmail.com>
2017-03-08 00:48:10 +0000

    serve bottle static files

--- dmlbottle.py
+++ dmlbottle.py
@@ -1,6 +1,9 @@
-from bottle import Bottle, route, run, template, get, post, request, static_file, response, debug
+from bottle import Bottle, jinja2_view, Jinja2Template, url, route, run, template, get, post, request, static_file, response, debug
+import functools
+import beaker
 
 app = Bottle()
+view = functools.partial(jinja2_view, template_lookup=['templates'])
 
 @route('/hello/<name>')   #127.0.0.1:8080/hello/dominic
 def hello(name):
@@ -9,6 +12,10 @@ def hello(name):
 @route('/')
 def index():
 	return template('index.html')
+
+@get("/templates/<filepath:re:.*\.html>")
+def html(filepath):
+	return static_file(filepath, root="./templates")
 
 @get("/css/<filepath:re:.*\.css>")
 def css(filepath):
